ae1bc571b849ac8372068bbd2ed56a5acceadbcd
Shaffer <liuxufchn@gmail.com>
2021-04-29 00:50:46 +0800

    HashMap putval

---
+++ src/com/shaffer/test/hashmap/Test.java
@@ -0,0 +1,21 @@
+package com.shaffer.test.hashmap;
+
+import java.util.HashMap;
+
+/**
+ * @program: javaSourceLearning
+ * @Date: 2021/4/22 1:25
+ * @Author: Shaffer
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        HashMap<String, Double> map = new HashMap<>();
+        map.put("K1", 0.1);
+        map.put("K2", 0.2);
+        map.put("K3", 0.3);
+        map.put("K4", 0.4);
+        System.out.println( 9 | 9 >>> 1);
+        System.out.println(9 >>> 2);
+    }
+}
---
+++ src/com/shaffer/test/string/StringTest.java
@@ -0,0 +1,16 @@
+package com.shaffer.test.string;
+
+/**
+ * @program: javaSourceLearning
+ * @Date: 2021/4/28 18:09
+ * @Author: Shaffer
+ * @Description:
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        String s = new String("aaa");
+        StringBuilder sb = new StringBuilder("bbb");
+        StringBuffer stringBuffer = new StringBuffer("ccc");
+    }
+}
